071597e6927471f96d6ffee259e7e24e374f14e0
FTC-06\Team Tardis <tardisftc2016@gmail.com>
2017-01-26 18:16:19 -0500

    Jan 26 Update Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBed_Andrew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBed_Andrew.java
@@ -12,6 +12,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
@@ -21,13 +22,24 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.firstinspires.ftc.teamcode.Teleop2017.steps.COLOR_FIND;
 import static org.firstinspires.ftc.teamcode.TestBed_Andrew.steps.FORWARD;
 import static org.firstinspires.ftc.teamcode.TestBed_Andrew.steps.NORMAL_DRIVE;
 import static org.firstinspires.ftc.teamcode.TestBed_Andrew.steps.ROTATE;
 import static org.firstinspires.ftc.teamcode.TestBed_Andrew.steps.WHITE_LINE;
 
+
+
 @TeleOp(name="TestBed-2016", group="TeleOp")
-public class TestBed_Andrew extends TardisOpModeAutonomous {
+
+public class TestBed_Andrew extends OpMode {
+    DcMotor m1; //Define dcMotor as m1
+    DcMotor m2; //Define dcMotor as m2
+    DcMotor m3; //Define dcMotor as m3
+    DcMotor m4; //Define dcMotor as m4
+    OpticalDistanceSensor odsSensor1; //Define optical distance sensor as odsSensor1
+    ModernRoboticsI2cGyro gyro;; //Define gyro sensor as gyro
+    ModernRoboticsI2cRangeSensor range; //Define range sensor as range
     public void init() { //Start of the initiation for autonomous
         m1 = hardwareMap.dcMotor.get("m1"); //Sets m1 to m3 in the config
         m2 = hardwareMap.dcMotor.get("m2"); //Sets m2 to m1 in the config
@@ -35,6 +47,8 @@ public class TestBed_Andrew extends TardisOpModeAutonomous {
         m4 = hardwareMap.dcMotor.get("m4"); //Sets m4 to m2 in the config
         m2.setDirection(DcMotor.Direction.REVERSE); //Reverses direction of m2
         m4.setDirection(DcMotor.Direction.REVERSE); //Reverses direction of m4
+
+        odsSensor1 = hardwareMap.opticalDistanceSensor.get("ods1"); //Sets odsSensor1 to ods1 in the config
 
         range = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "range"); //Sets range to range in the config
 
@@ -60,11 +74,16 @@ public class TestBed_Andrew extends TardisOpModeAutonomous {
         NORMAL_DRIVE,
         WHITE_LINE,
         FORWARD,
+        COLOR_FIND,
+        PUSH,
+        STOP,
+        SHOOT_BALL
     }
 
     boolean linearExtention = false;
     boolean beacon = false;
     boolean push = false;
+    int beaconSelect;
 
 
 
@@ -75,16 +94,24 @@ public class TestBed_Andrew extends TardisOpModeAutonomous {
     @Override
     public void loop() {
 
+        telemetry.addData(">", "Range (CM): " + range.getDistance(DistanceUnit.CM) + "\nzValue: " + gyro.getIntegratedZValue() + "\nHeading" + gyro.getHeading()); //Adds telemetry to debug
+        telemetry.update(); //Updates telemetry with new information
+
         float LUD = gamepad1.left_stick_y;
         float LRL = -gamepad1.left_stick_x;
         float R = gamepad1.right_stick_x;
         float LTrigger = gamepad1.left_trigger;
         double threshold = 0.1;
 
-        if (gamepad1.a) {
-            CURRENT_STEP = ROTATE;
+        if (gamepad1.b) {
+            CURRENT_STEP = steps.ROTATE;
+            beaconSelect = 0;
         }
-        if(gamepad1.x) {
+        if (gamepad1.x) {
+            CURRENT_STEP = steps.ROTATE;
+            beaconSelect = 1;
+        }
+        if(R > threshold || R < -threshold || LRL > threshold || LRL < -threshold || LUD > threshold || LUD < -threshold) {
             CURRENT_STEP = NORMAL_DRIVE;
         }
 
@@ -141,36 +168,84 @@ public class TestBed_Andrew extends TardisOpModeAutonomous {
 
             case ROTATE:
 
-                if(gyro.getIntegratedZValue() < -5) {
-                    m1.setPower(.09);
-                    m2.setPower(.09);
-                    m3.setPower(-.09);
-                    m4.setPower(-.09);
+                ///////
+                //RED//
+                ///////
+
+                if(gyro.getIntegratedZValue() < -5 && beaconSelect == 0) {
+                    m1.setPower(.07);
+                    m2.setPower(-.07);
+                    m3.setPower(.07);
+                    m4.setPower(-.07);
                     break;
                 }
-                if(gyro.getIntegratedZValue() > 5) {
-                    m1.setPower(-.09);
-                    m2.setPower(-.09);
-                    m3.setPower(.09);
-                    m4.setPower(.09);
+                if(gyro.getIntegratedZValue() > 5 && beaconSelect == 0) {
+                    m1.setPower(-.07);
+                    m2.setPower(.07);
+                    m3.setPower(-.07);
+                    m4.setPower(.07);
                     break;
                 }
-                if(gyro.getIntegratedZValue() > -5 && gyro.getIntegratedZValue() < 5) {
-                    CURRENT_STEP = WHITE_LINE;
+                if(gyro.getIntegratedZValue() > -5 && gyro.getIntegratedZValue() < 5 && beaconSelect == 0) {
+                    CURRENT_STEP = steps.WHITE_LINE;
+                    break;
+                }
+
+                ////////
+                //BLUE//
+                ////////
+
+                if(gyro.getIntegratedZValue() < -95 && beaconSelect == 1) {
+                    m1.setPower(.07);
+                    m2.setPower(-.07);
+                    m3.setPower(.07);
+                    m4.setPower(-.07);
+                    break;
+                }
+                if(gyro.getIntegratedZValue() > -85 && beaconSelect == 1) {
+                    m1.setPower(-.07);
+                    m2.setPower(.07);
+                    m3.setPower(-.07);
+                    m4.setPower(.07);
+                    break;
+                }
+                if(gyro.getIntegratedZValue() > -95 && gyro.getIntegratedZValue() < -85 && beaconSelect == 1) {
+                    CURRENT_STEP = steps.WHITE_LINE;
                     break;
                 }
 
             case WHITE_LINE:
 
-                if(odsSensor1.getRawLightDetected() < .5) {
+                ///////
+                //RED//
+                ///////
+
+                if(odsSensor1.getRawLightDetected() < .5 && beaconSelect == 0) {
                     m1.setPower(-.1);
                     m2.setPower(.1);
                     m3.setPower(.1);
                     m4.setPower(-.1);
                     break;
                 }
-                if(odsSensor1.getRawLightDetected() < .5) {
-                    CURRENT_STEP = FORWARD;
+
+                ////////
+                //BLUE//
+                ////////
+
+                if(odsSensor1.getRawLightDetected() < .5 && beaconSelect == 1) {
+                    m1.setPower(.1);
+                    m2.setPower(-.1);
+                    m3.setPower(-.1);
+                    m4.setPower(.1);
+                    break;
+                }
+
+                if(odsSensor1.getRawLightDetected() > .5) {
+                    m1.setPower(0); //Sets motor 1 to power 0 before next step
+                    m2.setPower(0); //Sets motor 2 to power 0 before next step
+                    m3.setPower(0); //Sets motor 3 to power 0 before next step
+                    m4.setPower(0); //Sets motor 4 to power 0 before next step
+                    CURRENT_STEP = steps.FORWARD;
                     break;
                 }
 
@@ -186,20 +261,20 @@ public class TestBed_Andrew extends TardisOpModeAutonomous {
                     break; //Exits switch statement
                 } //End of if statement
                 if (range.getDistance(DistanceUnit.CM) < 14) { //If the robot is to close to the beacon, back away
-                    m1.setPower(0); //Sets motor 1 to power -.2 to go backward because the robot is too close to the beacon
-                    m2.setPower(-.2); //Sets motor 2 to power -.2 to go backward because the robot is too close to the beacon
-                    m3.setPower(-.2); //Sets motor 3 to power -.2 to go backward because the robot is too close to the beacon
-                    m4.setPower(0); //Sets motor 4 to power -.2 to go backward because the robot is too close to the beacon
+                    m1.setPower(.1); //Sets motor 1 to power -.2 to go backward because the robot is too close to the beacon
+                    m2.setPower(.1); //Sets motor 2 to power -.2 to go backward because the robot is too close to the beacon
+                    m3.setPower(.1); //Sets motor 3 to power -.2 to go backward because the robot is too close to the beacon
+                    m4.setPower(.1); //Sets motor 4 to power -.2 to go backward because the robot is too close to the beacon
                     break; //Exits switch statement
                 } //End of if statement
                 if (range.getDistance(DistanceUnit.CM) > 14) { //If the robot is to far away from the beacon, move closer
-                    m1.setPower(.2); //Sets motor 1 to power .2 to go forward because the robot is too far from the beacon
-                    m2.setPower(0); //Sets motor 2 to power .2 to go forward because the robot is too far from the beacon
-                    m3.setPower(0); //Sets motor 3 to power .2 to go forward because the robot is too far from the beacon
-                    m4.setPower(.2); //Sets motor 4 to power .2 to go forward because the robot is too far from the beacon
+                    m1.setPower(-.1); //Sets motor 1 to power .2 to go forward because the robot is too far from the beacon
+                    m2.setPower(-.1); //Sets motor 2 to power .2 to go forward because the robot is too far from the beacon
+                    m3.setPower(-.1); //Sets motor 3 to power .2 to go forward because the robot is too far from the beacon
+                    m4.setPower(-.1); //Sets motor 4 to power .2 to go forward because the robot is too far from the beacon
                     break; //Exits switch statement
                 } //End of if statement
                 break; //Exits switch statement
         }
-        }
     }
+}
